33fec26b2eb6dd1047e497b9c86a3875014078cf
samir2692087 <146707734+samir2692087@users.noreply.github.com>
2026-07-15 07:00:07 +0530

    Add Gemini client for text generation

--- app/gemini_client.py
+++ app/gemini_client.py
@@ -1,1 +1,11 @@
+from google import genai
+from config import GEMINI_API_KEY
 
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+def generate_text(prompt: str):
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+    return response.text
