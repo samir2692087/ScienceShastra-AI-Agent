869f92c9eaad3a0811cc0549e66f18b7ccf937a0
samir2692087 <146707734+samir2692087@users.noreply.github.com>
2026-07-15 11:51:30 +0530

    Update model version in generate_text function

--- app/gemini_client.py
+++ app/gemini_client.py
@@ -5,7 +5,7 @@ client = genai.Client(api_key=GEMINI_API_KEY)
 
 def generate_text(prompt: str):
     response = client.models.generate_content(
-        model="gemini-2.5-flash",
+        model="gemini-2.0-flash",
         contents=prompt,
     )
     return response.text
